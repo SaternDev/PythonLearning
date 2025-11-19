4df630ee8db302fd1e79171d6f17f90aaa1d9d57
SaternDev <saterndev@gmail.com>
2025-11-19 15:02:48 +0100

    chore(examen): eliminar archivo de examen

---
+++ UnidadTrabajo3_Parte2/EjerciciosPracticar/Ejercicio10.py
@@ -0,0 +1,6 @@
+# Crea un programa que permita adivinar un número. La aplicación genera un número aleatorio del 1 al 100 
+# (utiliza la librería random).
+# A continuación, va pidiendo números y va respondiendo si el número a adivinar es mayor o menor que el introducido,
+# además de los intentos que te quedan (tienes 10 intentos para acertarlo). El programa termina cuando se acierta el número
+# (además te dice en cuantos intentos lo has acertado), si se llega al límite de intentos te muestra el número que había generado.
+
