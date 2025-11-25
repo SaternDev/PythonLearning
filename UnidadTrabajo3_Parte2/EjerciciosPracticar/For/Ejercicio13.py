a3012873fb045c9c9610e74658e3d854555f2d4b
SaternDev <saterndev@gmail.com>
2025-11-25 08:47:34 +0100

    feat(Ejercicio13 and 15 .py): Finished eje 15 and Eje 13

--- UnidadTrabajo3_Parte2/EjerciciosPracticar/For/Ejercicio13.py
+++ UnidadTrabajo3_Parte2/EjerciciosPracticar/For/Ejercicio13.py
@@ -5,12 +5,12 @@
 hourWork = 0
 totalHourWork = 0
 hourPay = 0
-totalHourPay = 0
+
+hourPay = round(float(input('A cuanto has conbrado las horas? \n')), 2)
 
 for _ in range(6):
     hourWork = round(float(input('Cuantas horas has trabajado? \n')),1)
     totalHourWork += hourWork
     hourPay = round(float(input('A cuanto has conbrado las horas? \n')), 2)
-    totalHourPay += hourPay
 
-print(f'Ha hecho: {totalHourWork}h totales en la semana y ha cobrado: {totalHourPay}€')
+print(f'Ha hecho: {totalHourWork}h totales en la semana y ha cobrado: {hourPay * totalHourWork}€')
