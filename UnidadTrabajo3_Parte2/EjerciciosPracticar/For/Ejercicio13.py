219b60dbc6b7514f3a1b9a5b650316e429edfef6
SaternDev <saterndev@gmail.com>
2025-11-25 08:24:46 +0100

    feat(ejercicio13.py): implementada solución ejercicio

---
+++ UnidadTrabajo3_Parte2/EjerciciosPracticar/For/Ejercicio13.py
@@ -0,0 +1,16 @@
+#Una empresa quiere tener el registro de las horas que trabaja diariamente un empleado durante la semana (seis días)
+#  para determinar el total de éstas, así como el sueldo que recibirá por las horas trabajadas. 
+# Por tanto, deberás pedir por teclado cuantas horas ha trabajado cada día, así como a cuánto cobra la hora. El precio por hora es único.
+
+hourWork = 0
+totalHourWork = 0
+hourPay = 0
+totalHourPay = 0
+
+for _ in range(2):
+    hourWork = round(float(input('Cuantas horas has trabajado? \n')),1)
+    totalHourWork += hourWork
+    hourPay = round(float(input('A cuanto has conbrado las horas? \n')), 2)
+    totalHourPay += hourPay
+
+print(f'Ha hecho: {totalHourWork}h totales en la semana y ha cobrado: {totalHourPay}€')
