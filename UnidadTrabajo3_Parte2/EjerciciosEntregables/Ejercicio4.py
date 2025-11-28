113b94acd4ab56c96b106ff275852585ad18ff5e
SaternDev <saterndev@gmail.com>
2025-11-28 09:53:05 +0100

    fix(entregable-4): corregir errores de código en la implementación

--- UnidadTrabajo3_Parte2/EjerciciosEntregables/Ejercicio4.py
+++ UnidadTrabajo3_Parte2/EjerciciosEntregables/Ejercicio4.py
@@ -12,10 +12,9 @@ month = actualDate.month
 day = actualDate.day
 
 for _ in range(7):
-    if fichero.endswith(str(day)):
-        day += 1
     if day == 32:
         month += 1
         day = 1
     fichero = 'backup_' + str(actualDate.year) + '_' + str(month) + '_' + str(day) + '.zip'
+    day += 1
     print(fichero)
